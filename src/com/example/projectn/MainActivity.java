d87083cc9e8d6fe66e4712e19e4768189a5e0516
student <student@student15>
2017-08-25 11:20:13 +0900

    test

---
+++ src/com/example/projectn/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.projectn;
+
+import com.example.projectn.R;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements LocationListener  {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Button okButton = (Button)findViewById(R.id.gacha);
+		okButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				LinearLayout linearLayout = new LinearLayout(getBaseContext());     //１．リニアレイアウトをインスタンス化
+			    linearLayout.setBackgroundColor(Color.WHITE);           //２．背景色の指定
+			    
+			    setContentView(linearLayout);                           //３．レイアウトをコンテントに表示する
+				
+			    //コードでテキストビューを生成＆セット
+		        TextView textView = new TextView(getBaseContext());                 //４．テキストビューのインスタンスを生成
+		        textView.setText("TEST OK!");                           //５．表示する文字をセット
+		        textView.setTextSize(50);                               //６．テキストのサイズをセット
+		        textView.setTextColor(Color.RED);                       //７．テキストの色をセット
+		        linearLayout.addView(textView);                         //８．レイアウトにテキストビューを追加して表示する
+			}
+		});
+			    
+	    LocationManager manager = (LocationManager)getSystemService(LOCATION_SERVICE);
+	    manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		double latitude, longtitude;
+		
+		latitude = location.getLatitude();
+		longtitude = location.getLongitude();
+		
+		TextView textView2 = (TextView)findViewById(R.id.textView1);
+		textView2.setText("緯度:" + latitude + ", 経度:" + longtitude);
+
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+}
