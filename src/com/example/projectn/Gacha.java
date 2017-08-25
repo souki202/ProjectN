139437608687ec056726d943e625799e6651319d
student <student@student15>
2017-08-25 13:12:41 +0900

    余計なもの削除

--- src/com/example/projectn/Gacha.java
+++ src/com/example/projectn/Gacha.java
@@ -5,13 +5,9 @@ import com.example.projectn.MainActivity;
 import android.content.Context;
 
 public class Gacha {
-	private Handler handler;
-	private Context context;
 
 	public Gacha(MainActivity mainActivity) {
 		// TODO Auto-generated constructor stub
-		handler = new Handler();
-	    this.context = context;
 	}
 
 	public int execute() {
