2ad4bd98611bf67db51b8485ba74faef96aa68a5
student <student@student14>
2017-08-25 11:44:29 +0900

    test

--- src/com/example/projectn/MainActivity.java
+++ src/com/example/projectn/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+//
 public class MainActivity extends Activity implements LocationListener  {
 	private float growthStage = 0.f;
 	private double lastLa = 0.0, lastLong = 0.0;
